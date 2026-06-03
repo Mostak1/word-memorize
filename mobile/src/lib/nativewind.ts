7331e9ceb72705794542cd5a2ca59f50a1eaa536
Ignatious <ignatiousr80@gmail.com>
2026-06-03 17:58:07 +0600

    updatd

--- mobile/src/lib/nativewind.ts
+++
@@ -1,7 +0,0 @@
-import { StyleSheet } from "nativewind";
-
-type NativeWindStyleSheet = typeof StyleSheet & {
-  setFlag?: (name: string, value: string) => void;
-};
-
-(StyleSheet as NativeWindStyleSheet).setFlag?.("darkMode", "class");
